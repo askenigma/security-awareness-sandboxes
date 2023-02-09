1b02da1e5e385ad282ca964856c40370f2828e88
Harley Sugarman <harleysugarman@gmail.com>
2023-02-09 00:01:24 -0500

    move message data to separate file

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -23,6 +23,8 @@ import {
   XMarkIcon,
 } from "@heroicons/react/24/outline";
 
+import { messages, message } from "assets/email_data";
+
 const user = {
   name: "Whitney Francis",
   email: "whitney.francis@example.com",
@@ -54,144 +56,6 @@ const userNavigation = [
   { name: "Your Profile", href: "#" },
   { name: "Sign out", href: "#" },
 ];
-const messages = [
-  {
-    id: 1,
-    subject: "Request for Meeting on Friday, February 11th",
-    sender: "John Doe",
-    href: "#",
-    date: "1d ago",
-    datetime: "2023-02-07T10:00",
-    preview:
-      "Dear Jaime, I hope this email finds you well. I wanted to reach out and request a meeting with you on Friday, February 11th. Let me know if this time works for you and I'll send over an agenda.",
-  },
-  {
-    id: 2,
-    subject: "Important Updates on Project X",
-    sender: "Jane Smith",
-    href: "#",
-    date: "3d ago",
-    datetime: "2023-02-06T15:00",
-    preview:
-      "Good afternoon Jaime, I hope you're doing well. I wanted to provide you with an update on Project X. Our team has made significant progress and I would love to go over the details with you.",
-  },
-  {
-    id: 3,
-    subject: "Follow-Up on Proposal Submission",
-    sender: "Michael Johnson",
-    href: "#",
-    date: "5d ago",
-    datetime: "2023-02-04T09:00",
-    preview:
-      "Dear Jaime, I hope this email finds you well. I wanted to follow up on the proposal we submitted last week. Have you had a chance to review it and discuss it with your team? I'd love to hear your thoughts and answer any questions you may have.",
-  },
-  {
-    id: 4,
-    subject: "Reminder: Conference Call Tomorrow at 10am",
-    sender: "Sarah Brown",
-    href: "#",
-    date: "1d ago",
-    datetime: "2023-02-07T10:00",
-    preview:
-      "Hello Jaime, Just wanted to send a quick reminder about our conference call tomorrow at 10am. Please let me know if there are any changes to the schedule. Looking forward to catching up with you!",
-  },
-  {
-    id: 5,
-    subject: "Invitation to Networking Event Next Month",
-    sender: "William Davis",
-    href: "#",
-    date: "7d ago",
-    datetime: "2023-01-31T17:00",
-    preview:
-      "Dear Jaime, I hope this email finds you well. I wanted to extend an invitation to a networking event that I'll be attending next month. It's a great opportunity to connect with like-minded professionals and learn about new trends in our industry. Let me know if you're interested and I'll send over more details.",
-  },
-  {
-    id: 6,
-    subject: "Action Required: Approve Expense Report",
-    sender: "David Wilson",
-    href: "#",
-    date: "2d ago",
-    datetime: "2023-02-07T12:00",
-    preview:
-      "Good morning Jaime, I hope this email finds you well. I wanted to remind you that there's an expense report that requires your approval. Could you please take a look and let me know if everything is in order? Thank you for your help!",
-  },
-  {
-    id: 7,
-    subject: "Update on  Upcoming Trade Show Participation",
-    sender: "Emily Parker",
-    href: "#",
-    date: "4d ago",
-    datetime: "2023-02-05T14:00",
-    preview:
-      "Dear Jaime, I hope you're having a great week. I wanted to provide an update on our upcoming trade show participation. Our team has been working hard to put together a great booth and we're looking forward to showcasing our products and services. Let me know if you have any questions or would like to meet up at the show.",
-  },
-  {
-    id: 8,
-    subject: "Confirmation of Travel Arrangements",
-    sender: "Brian Anderson",
-    href: "#",
-    date: "6d ago",
-    datetime: "2023-02-03T16:00",
-    preview:
-      "Good afternoon Jaime, I hope this email finds you well. I just wanted to confirm the travel arrangements for our upcoming business trip. Our flight is scheduled for next Monday at 8am and I have arranged for a car service to pick us up from the airport. Let me know if there are any changes to the schedule.",
-  },
-  {
-    id: 9,
-    subject: "Deadline Extension Request for Proposal",
-    sender: "Karen Thompson",
-    href: "#",
-    date: "8d ago",
-    datetime: "2023-02-01T11:00",
-    preview:
-      "Dear Jaime, I hope you're doing well. I wanted to reach out and request an extension on the deadline for our proposal. We've run into some unexpected obstacles and need a bit more time to complete the project. I understand that this may cause some inconvenience, but I would greatly appreciate your understanding and support in this matter.",
-  },
-  {
-    id: 10,
-    subject: "Reminder: Performance Review Meeting Next Week",
-    sender: "Amy Lee",
-    href: "#",
-    date: "9d ago",
-    datetime: "2023-01-31T10:00",
-    preview:
-      "Hello Jaime, Just wanted to send a quick reminder about our performance review meeting next week. I have received your updated performance report and I'm looking forward to discussing your progress and setting new goals for the upcoming quarter. Let me know if there are any changes to the schedule.",
-  },
-];
-const message = {
-  subject: "Re: New pricing for existing customers",
-  sender: "joearmstrong@example.com",
-  status: "Open",
-  items: [
-    {
-      id: 1,
-      author: "Joe Armstrong",
-      date: "Yesterday at 7:24am",
-      datetime: "2021-01-28T19:24",
-      body: "<p>Thanks so much! Can't wait to try it out.</p>",
-    },
-    {
-      id: 2,
-      author: "Monica White",
-      date: "Wednesday at 4:35pm",
-      datetime: "2021-01-27T16:35",
-      body: `
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Malesuada at ultricies tincidunt elit et, enim. Habitant nunc, adipiscing non fermentum, sed est a, aliquet. Lorem in vel libero vel augue aliquet dui commodo.</p>
-        <p>Nec malesuada sed sit ut aliquet. Cras ac pharetra, sapien purus vitae vestibulum auctor faucibus ullamcorper. Leo quam tincidunt porttitor neque, velit sed. Tortor mauris ornare ut tellus sed aliquet amet venenatis condimentum. Convallis accumsan et nunc eleifend.</p>
-        <p><strong style="font-weight: 600;">Monica White</strong><br/>Customer Service</p>
-      `,
-    },
-    {
-      id: 3,
-      author: "Joe Armstrong",
-      date: "Wednesday at 4:09pm",
-      datetime: "2021-01-27T16:09",
-      body: `
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Malesuada at ultricies tincidunt elit et, enim. Habitant nunc, adipiscing non fermentum, sed est a, aliquet. Lorem in vel libero vel augue aliquet dui commodo.</p>
-        <p>Nec malesuada sed sit ut aliquet. Cras ac pharetra, sapien purus vitae vestibulum auctor faucibus ullamcorper. Leo quam tincidunt porttitor neque, velit sed. Tortor mauris ornare ut tellus sed aliquet amet venenatis condimentum. Convallis accumsan et nunc eleifend.</p>
-        <p>– Joe</p>
-      `,
-    },
-  ],
-};
 
 function classNames(...classes: any) {
   return classes.filter(Boolean).join(" ");
