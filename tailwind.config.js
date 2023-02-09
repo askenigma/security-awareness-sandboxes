e76871f2311c1c136d19c0fd47c788c97542f574
Harley Sugarman <harleysugarman@gmail.com>
2023-02-08 23:00:52 -0500

    initial commit of tailwind inbox

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        cyan: colors.cyan,
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/line-clamp")],
+};
