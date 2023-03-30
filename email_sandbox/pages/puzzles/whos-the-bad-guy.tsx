f12d5cf643e4b5ca0966385f2a09caf423654ed3
Harley Sugarman <harleysugarman@gmail.com>
2023-03-30 14:40:07 -0400

    escape quote

--- email_sandbox/pages/puzzles/whos-the-bad-guy.tsx
+++ email_sandbox/pages/puzzles/whos-the-bad-guy.tsx
@@ -5,7 +5,7 @@ export default function Puzzle() {
   return (
     <>
       <Head>
-        <title>Who's The Bad Guy?</title>
+        <title>Who&apos;s The Bad Guy?</title>
         <meta name="description" content="Email Sandbox" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
